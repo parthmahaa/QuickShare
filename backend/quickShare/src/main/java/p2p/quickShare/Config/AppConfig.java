bdba1354c82e466cb5ac068f9a9f1e34d74a4aeb
Parth Maha <parthmaha9@gmail.com>
2025-09-23 01:14:30 +0530

    update beans

--- backend/quickShare/src/main/java/p2p/quickShare/Config/AppConfig.java
+++ backend/quickShare/src/main/java/p2p/quickShare/Config/AppConfig.java
@@ -13,30 +13,45 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-    @Value("${aws.accessKeyId}")
-    private String awsAccessKey;
-
-    @Value("${aws.secretKey}")
-    private String awsSecretKey;
-
-    @Value("${aws.region}")
-    private String awsRegion;
+//    @Value("${aws.accessKeyId}")
+//    private String awsAccessKey;
+//
+//    @Value("${aws.secretKey}")
+//    private String awsSecretKey;
+//
+//    @Value("${aws.region}")
+//    private String awsRegion;
+//
+//    @Bean
+//    public AmazonS3 amazonS3Client() {
+//        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+//        return AmazonS3ClientBuilder.standard()
+//                .withRegion(awsRegion)
+//                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+//                .build();
+//    }
+//    @Bean
+//    public AmazonDynamoDB amazonDynamoDB() {
+//        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+//        return AmazonDynamoDBClientBuilder.standard()
+//                .withRegion(awsRegion)
+//                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+//                .build();
+//    }
+//
+//    @Bean
+//    public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB) {
+//        return new DynamoDBMapper(amazonDynamoDB);
+//    }
 
     @Bean
-    public AmazonS3 amazonS3Client() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(awsRegion)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-                .build();
+    public AmazonS3 s3Client() {
+        return AmazonS3ClientBuilder.standard().build();
     }
+
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
-        return AmazonDynamoDBClientBuilder.standard()
-                .withRegion(awsRegion)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-                .build();
+        return AmazonDynamoDBClientBuilder.standard().build();
     }
 
     @Bean
